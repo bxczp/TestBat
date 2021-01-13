5d451e743a62978bedf8de4c734f5bb7a17edd01
bxczp <bxczp@hotmail.com>
2021-01-13 21:10:01 +0800

    init

---
+++ test.bat
@@ -0,0 +1,13 @@
+@echo off
+
+wmic process where (commandline like "%%ALL-UNNAMED%%" and NAME="javaw.exe") get ProcessID | findstr [0-9] > temp.txt
+
+for /F "delims=" %%a in (temp.txt) do (
+  set "pid=%%a"
+)
+
+"C:\Program Files\Java\jdk1.8.0_241\bin\jinfo" %pid%  > C:\Users\bxczp\Desktop\test2.txt
+
+del temp.txt /q
+
+
